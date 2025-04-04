bc838a3d7837a90fa7596ea6e9c663b18ea29cea
rvincken <2403211vincken@zuyd.nl>
2025-04-04 11:49:16 +0200

    Scripts moved to /src and unnessecary installdependencies.bat removed

--- installdependencies.bat
+++
@@ -1,8 +0,0 @@
-(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe', 'C:\Tools\python-3.12.5.exe')
-
-C:\Tools\python-3.12.5.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
-
-pip install --upgrade pip
-pip install opencv-python mediapipe
